7ab185f1a533a6712dedb8bb60ce87c0c31b3104
saswat0 <32325136+saswat0@users.noreply.github.com>
2020-04-07 19:35:30 +0530

    Add files via upload

---
+++ AdaMatting/train.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+python main.py \
+    --mode=train \
+    --valid_portion=5 \
+    --batch_size=16 \
+    --epochs=120 \
+    --lr=0.0001 \
+    --cuda \
+    --gpu=1,5,6,7 \
+    --write_log \
+    --save_ckpt
+    # --resume \
+
+    #--resume=model/stage1/ckpt_e1.pth \
