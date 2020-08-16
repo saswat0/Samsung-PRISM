89c911946f7d14a34c40f4638cde0ec819cc22e4
Saswat <saswatsubhajyotimallick@gmail.com>
2020-08-16 12:38:18 +0530

    added dataset download links

---
+++ AdaMatting_v1/download.sh
@@ -0,0 +1,4 @@
+gdown https://drive.google.com/uc?id=1Tz99lhYJPH2WQ7LutQU0-CUFT_lBYZpF -P Datasets/
+gdown https://drive.google.com/uc?id=1Vc_7CLkkPFaZjoKM-0gv3DfdZQupkh8w -P Datasets/
+wget -O http://host.robots.ox.ac.uk/pascal/VOC/voc2008/VOCtrainval_14-Jul-2008.tar -P Datasets/
+gdown https://drive.google.com/uc?id=1vFrZ6bYAqF3KWLq9ckc1EaxE77YIxohG -P Datasets/
